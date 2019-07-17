46977ae223053fd68c15c5c6819bd64c239bc804
shirokuro <46kuro212@gmail.com>
2019-07-17 17:51:15 +0900

    chore: make bukkit's dependency declaration 'compileOnly'

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.bukkit", "bukkit", "1.12.2-R0.1-SNAPSHOT")
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.bukkit", "bukkit", "1.12.2-R0.1-SNAPSHOT")
     implementation("io.arrow-kt", "arrow-core-data", "0.9.0")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.5.0")
 }
