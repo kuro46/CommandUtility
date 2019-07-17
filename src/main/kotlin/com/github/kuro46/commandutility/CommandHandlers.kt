7e1fdfd11febe5e6654f5ced03843be4cae32ded
shirokuro <46kuro212@gmail.com>
2019-07-17 14:44:10 +0900

    fix: fix StackOverFlowError when calling MutableCommandEntry#toImmutable

--- src/main/kotlin/com/github/kuro46/commandutility/CommandHandlers.kt
+++ src/main/kotlin/com/github/kuro46/commandutility/CommandHandlers.kt
@@ -44,19 +44,30 @@ private class MutableCommandTreeEntry(
 ) {
 
     val children = HashMap<String, MutableCommandTreeEntry>()
+    val isRoot = parent == null
 
     fun toImmutable(): CommandTreeEntry {
-        val map = HashMap<String, CommandTreeEntry>(children.size)
-
-        for ((key, value) in children) {
-            map[key] = value.toImmutable()
+        if (!isRoot) {
+            throw IllegalStateException("This tree is not a root.")
         }
 
-        return CommandTreeEntry(
+        return toImmutable(null)
+    }
+
+    private fun toImmutable(parent: CommandTreeEntry?): CommandTreeEntry {
+        val map = HashMap<String, CommandTreeEntry>(children.size)
+
+        val immutableOfThis = CommandTreeEntry(
             command,
-            parent?.toImmutable(),
+            parent,
             map
         )
+
+        for ((key, value) in children) {
+            map[key] = value.toImmutable(immutableOfThis)
+        }
+
+        return immutableOfThis
     }
 }
 
