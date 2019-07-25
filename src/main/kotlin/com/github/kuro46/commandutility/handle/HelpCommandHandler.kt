0431a2e36885dba76b8441e51fe4a3517970f4b5
shirokuro <46kuro212@gmail.com>
2019-07-25 21:07:45 +0900

    fix: fix issue of HelpCommandHandler (#10)

--- src/main/kotlin/com/github/kuro46/commandutility/handle/HelpCommandHandler.kt
+++ src/main/kotlin/com/github/kuro46/commandutility/handle/HelpCommandHandler.kt
@@ -3,7 +3,7 @@ package com.github.kuro46.commandutility.handle
 import com.github.kuro46.commandutility.syntax.CommandSyntaxBuilder
 import org.bukkit.command.CommandSender
 
-class HelpCommandHandler(val commandTree: CommandTree) : CommandHandler() {
+class HelpCommandHandler(val sectionsToSend: CommandSections) : CommandHandler() {
 
     override val commandSyntax = CommandSyntaxBuilder().build()
 
@@ -15,6 +15,11 @@ class HelpCommandHandler(val commandTree: CommandTree) : CommandHandler() {
         commandSections: CommandSections,
         args: Map<String, String>
     ) {
+        val commandTreeEntry = caller.commandTree.findTree(sectionsToSend)
+        val commandTree = when (commandTreeEntry) {
+            is CommandTreeRoot -> throw IllegalArgumentException("Tree: '$commandSections' not found!")
+            is CommandTree -> commandTreeEntry
+        }
         commandTree.forEach {
             val command = it.command
             if (command == null || command.handler is FallbackCommandHandler) {
