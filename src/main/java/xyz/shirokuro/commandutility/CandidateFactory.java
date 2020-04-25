d54ef72e2fcb0d3e8e41ef7c1e1dc0a0bc1f0f1b
shirokuro <46kuro212@gmail.com>
2020-04-25 14:53:57 +0900

    Remove unused classes

--- src/main/java/xyz/shirokuro/commandutility/CandidateFactory.java
+++
@@ -1,9 +0,0 @@
-package xyz.shirokuro.commandutility;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface CandidateFactory {
-
-    List<String> create(final String currentValue);
-}
