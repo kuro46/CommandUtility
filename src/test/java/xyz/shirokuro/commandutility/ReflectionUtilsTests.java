c928696dfbfa488ccd3385d90e62e04ced1a5598
shirokuro <46kuro212@gmail.com>
2020-04-28 20:56:35 +0900

    Add tests for ReflectionUtils

---
+++ src/test/java/xyz/shirokuro/commandutility/ReflectionUtilsTests.java
@@ -0,0 +1,55 @@
+package xyz.shirokuro.commandutility;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public final class ReflectionUtilsTests {
+
+    @Test
+    public void equalsMethodParamsTestEqual() {
+        Method m = getMethod("oneParameterMethod", Object.class);
+        ReflectionUtils.equalsMethodParams(m, Object.class);
+    }
+
+    private Method getMethod(final String name, final Class<?>... params) {
+        try {
+            return TargetClass.class.getDeclaredMethod(name, params);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void equalsMethodParamsTestNotEqual() {
+        Method m = getMethod("publicMethod");
+        ReflectionUtils.equalsMethodParams(m, Object.class);
+    }
+
+    @Test
+    public void assertPublicTestPublicMethod() {
+        Method m = getMethod("publicMethod");
+        assertDoesNotThrow(() -> ReflectionUtils.assertPublic(m));
+    }
+
+    @Test
+    public void assertPublicTestPrivateMethod() {
+        Method m = getMethod("privateMethod");
+        assertThrows(IllegalArgumentException.class, () -> ReflectionUtils.assertPublic(m));
+    }
+
+    @SuppressWarnings("unused")
+    private static class TargetClass {
+        private static void privateMethod() {
+        }
+
+        public static void publicMethod() {
+        }
+
+        public static void oneParameterMethod(Object o) {
+        }
+    }
+}
