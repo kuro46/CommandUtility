4f20e6ea331980e6429a35b6690eb544d28bd225
shirokuro <46kuro212@gmail.com>
2020-09-22 07:15:01 +0900

    Add a feature that able to specify permission for HelpCommandHandler

--- src/main/java/dev/shirokuro/commandutility/HelpCommandHandler.java
+++ src/main/java/dev/shirokuro/commandutility/HelpCommandHandler.java
@@ -3,6 +3,8 @@ package dev.shirokuro.commandutility;
 import java.util.StringJoiner;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permissible;
 
 /**
  * {@code HelpCommandHandler} is a {@code CommandHandler} to display help messages of all commands.
@@ -11,6 +13,7 @@ public final class HelpCommandHandler implements CommandHandler {
 
     private final String header;
     private final String footer;
+    private final String requiredPermission;
 
     /**
      * Constructs {@code HelpCommandHandler} with specified header and footer.
@@ -18,22 +21,31 @@ public final class HelpCommandHandler implements CommandHandler {
      * @param header header of this command
      * @param footer footer of this command
      */
-    public HelpCommandHandler(final String header, final String footer) {
+    public HelpCommandHandler(final String header, final String footer, final String requiredPermission) {
         this.header = header;
         this.footer = footer;
+        this.requiredPermission = requiredPermission;
+    }
+
+    public HelpCommandHandler(final String requiredPermission) {
+        this(null, null, requiredPermission);
     }
 
     public HelpCommandHandler() {
-        this(null, null);
+        this(null, null, null);
     }
 
     @Override
     public void execute(final ExecutionData data) {
-        final BranchNode rootBranch = data.getGroup().getRoot();
         final CommandSender sender = data.getSender();
+        if (requiredPermission != null && !sender.hasPermission(requiredPermission)) {
+            sender.sendMessage("You do not have permission");
+            return;
+        }
         if (header != null) {
             sender.sendMessage(header);
         }
+        final BranchNode rootBranch = data.getGroup().getRoot();
         for (final CommandNode commandNode : rootBranch.walkNodeTree()) {
             final Command command = commandNode.getCommand();
             final StringJoiner sj = new StringJoiner(" ");
